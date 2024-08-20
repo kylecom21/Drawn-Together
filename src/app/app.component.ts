11048191e10d17435b9ad1788ed2071129d45bb5
codyhighroller <123840075+codyhighroller@users.noreply.github.com>
2024-08-20 11:10:13 +0100

    whiteboard component v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,22 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WebsocketTestComponent } from './websocket-test/websocket-test.component';
 import { TimerComponent } from './timer/timer.component';
+import { WhiteboardComponent } from './whiteboard/whiteboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, WebsocketTestComponent, TimerComponent],
+  imports: [
+    CommonModule,
+    WebsocketTestComponent,
+    TimerComponent,
+    WhiteboardComponent,
+  ],
   template: `
     <h1>{{ title }}</h1>
     <app-websocket-test></app-websocket-test>
     <app-timer></app-timer>
+    <app-whiteboard></app-whiteboard>
   `,
   styleUrl: './app.component.css',
 })
