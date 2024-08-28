f1ea9854bc3a4f9912bd3401f75b8a37b487f985
Kyle Compton <kylecompton@hotmail.co.uk>
2024-08-28 13:56:25 +0100

    centered title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ import { WordComponent } from './word/word.component';
     <div class="min-h-screen bg-gray-100 flex flex-col">
       <header class="bg-white shadow text-center">
         <div
-          class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center"
+          class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-center"
         >
           <h1 class="text-2xl font-bold text-gray-900">Drawn Together</h1>
           <!-- <nav>
